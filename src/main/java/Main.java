ff7e2737a30071ab05b9783acc9e2b9d27a052dd
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-23 11:26:35 -0400

    Created additional method in main class to facilitate the printing of an arraylist created our of objects that inherit from the Person superclass

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 import ABC.C;
 import Persons.Person;
 import Persons.Student;
+import Persons.Teacher;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
@@ -13,15 +16,26 @@ public class Main {
     //ABC Assignment --- End
 
     //Persons and Subclasses Assignment --- Start
-        Person patrick = new Person("Patrick Rallings", "3633 Nancy Creek Rd");
-        Person matt = new Person("Matt Pachapa", "123 Ansley Rd");
-        System.out.println(patrick.toString()+matt.toString());
-
-        Student patrick2 = new Student("Patrick Rallings", "3633 Nancy Creek Rd");
-        Student matt2 = new Student("Matt Pachapa", "123 Ansley Rd");
-        System.out.println(patrick2.toString()+matt2.toString());
-        patrick2.study();
-        System.out.println(patrick2.toString()+matt2.toString());
+        Student patrick = new Student("Patrick Rallings", "3633 Nancy Creek Rd");
+        Student matt = new Student("Matt Pachapa", "123 Ansley Rd");
+        Teacher hector = new Teacher("Hector Gonzalez", "123 Ansley Rd", 104);
+        int i = 0;
+        while (i < 25){
+            patrick.study();
+            i++;
+        }
+        ArrayList<Person> persons = new ArrayList<>();
+        persons.add(patrick);
+        persons.add(matt);
+        persons.add(hector);
+        printPersons(persons);
         //Persons and Subclasses Assignment --- End
+    }
+    public static void printPersons(ArrayList<Person> persons){
+        String s = "";
+        for (Person person : persons){
+            s += person.toString();
+        }
+        System.out.println(s);
     }
 }
