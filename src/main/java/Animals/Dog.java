4595321a71303277a22dbf78ea2390172bd4bd7f
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-23 16:24:29 -0400

    Implemented Animal abstract class to specific animal type: Dog

---
+++ src/main/java/Animals/Dog.java
@@ -0,0 +1,15 @@
+package Animals;
+
+public class Dog extends Animal{
+    public Dog(String name) {
+        super(name);
+    }
+
+    public Dog() {
+        super("Dog");
+    }
+
+    public void bark(){
+        System.out.println("Bark! Bark!");
+    }
+}
