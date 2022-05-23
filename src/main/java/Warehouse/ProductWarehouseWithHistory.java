eff0b5b79737d50a935963811bdba2c430e32c48
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-23 14:58:44 -0400

    Overrode superclass' methods for adding and subtracting products (to factor in history)

--- src/main/java/Warehouse/ProductWarehouseWithHistory.java
+++ src/main/java/Warehouse/ProductWarehouseWithHistory.java
@@ -2,10 +2,43 @@ package Warehouse;
 
 public class ProductWarehouseWithHistory extends ProductWarehouse{
 
-    ChangeHistory history;
+    public ChangeHistory history;
 
-    public ProductWarehouseWithHistory(String productName, double capacity) {
+    public ProductWarehouseWithHistory(String productName, double capacity, double initialBalance) {
         super(productName, capacity);
+        this.balance  = initialBalance;
+        history = new ChangeHistory(initialBalance);
+    }
+
+    @Override
+    public void addToWarehouse(double amountToAdd){
+        if (amountToAdd < 0){
+        }
+        else if (amountToAdd <= howMuchSpaceLeft()){
+            balance += amountToAdd;
+            history.add(getBalance());
+        } else {
+            balance = getCapacity();
+            history.add(getBalance());
+        }
+    }
+
+    @Override
+    public void takeFromWarehouse(double amountToTake){
+        if (amountToTake < 0){
+            System.out.println("Amount can't be negative.");
+        }
+        else if (amountToTake <= balance){
+            balance -= amountToTake;
+            history.add(getBalance());
+        } else {
+            balance = 0;
+            history.add(getBalance());
+        }
+    }
+
+    public void printAnalysis(){
+        System.out.println(history.toString());
     }
 
 
