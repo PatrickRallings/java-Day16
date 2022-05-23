1feec9d264a23a0562b9c3235c1e2cd77cc28685
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-23 16:03:09 -0400

    Added method to iterate through herdList and move each Movable object (while converting toString in output)

--- src/main/java/Herds/Herd.java
+++ src/main/java/Herds/Herd.java
@@ -1,11 +1,30 @@
 package Herds;
 
+import java.util.ArrayList;
+
 public class Herd implements Movable{
 
+    ArrayList<Movable> herdList;
 
+    public Herd(){
+        herdList = new ArrayList<>();
+    }
 
+    public void addToHerd(Movable movable){
+        herdList.add(movable);
+    }
 
     public void move(int dx, int dy) {
+        for(Movable movable : herdList)
+            movable.move(dx, dy);
+    }
 
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Movable movable : herdList){
+            s.append(movable.toString()).append("\n");
+        }
+        return s.toString();
     }
 }
